420dd78a74b79e7413d2e20f2c09deda260f8b25
Aarush Gupta <aarushgupta2735@gmail.com>
2025-10-21 16:52:41 +0000

    Second Commit: Integrated with local postgre database

--- server.js
+++ server.js
@@ -5,20 +5,42 @@ const app = express()
 //.json middleware
 app.use(express.json());
 
-let todos = [];
+const pgp = require('pg-promise')()
+const connectionString = 'postgres://postgres:postgres@localhost:5432/mydb';
+const db = pgp(connectionString)
 
-app.get('/todos',(req,res)=>{
-    res.send(todos);
+app.get('/todos',async(req,res)=>{
+    try{
+        const todos = await db.any('SELECT * FROM todos'); //any returns NULL too
+        res.send(todos);
+    }
+    catch(error){
+        res.status(500).json({error:'Failed to fetch todos'})
+    }
 })
 
-app.post('/todos',(req,res)=>{
-    todos.push(req.body);
-    res.send('To-do added')
+
+app.post('/todos',async (req,res)=>{
+    const {title,completed} = req.body;
+    try{
+        const newTodo = await db.one('INSERT INTO todos (title,completed) VALUES ($1,$2) RETURNING *',[title,completed])
+        res.status(201).json(newTodo);
+    }
+    catch(error){
+        console.error('Database error:',error);
+        res.status(600).json({error:'Could not post to-do'})
+    }
 })
 
-app.delete('/todos/:deleteId',(req,res)=>{
-    delete todos[req.params.deleteId];
-    res.send('Last To-do Deleted')
+app.delete('/todos/:deleteId',async (req,res)=>{
+    const id = req.params.deleteId;
+    try{
+        const deleteTodo = await db.one('DELETE FROM todos WHERE id = $1 RETURNING *',[id]);
+        res.status(201).json(deleteTodo);
+    }
+    catch(error){
+        console.error('Could not delete. Error:',error);
+    }
 })
 
 app.listen(port,()=>{
